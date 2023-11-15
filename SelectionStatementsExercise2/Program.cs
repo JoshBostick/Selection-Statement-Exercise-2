f8048a5fed7401dd5f6f21de38671dc8581305d9
Josh Bostick <jbostick99@gmail.com>
2023-11-15 10:49:25 -0600

    Finished Exercise 2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite school subject?");
+            var favSubject = Console.ReadLine();
+
+            switch (favSubject.ToLower()) 
+            {
+                case "math":
+                case "geometry":
+                case "calculus":
+                    Console.WriteLine("I love math!");
+                    break;
+                case "science":
+                case "biology":
+                case "chemistry":
+                    Console.WriteLine("Science is Awesome!");
+                    break;
+                case "history":
+                    Console.WriteLine("History is fun!");
+                    break;
+                case "english":
+                    Console.WriteLine("English is hard");
+                    break;
+                case "spanish":
+                    Console.WriteLine("That's cool!");
+                    break;
+                default:
+                    Console.WriteLine("I don't know that subject");
+                    break;
+            }
         }
     }
 }
